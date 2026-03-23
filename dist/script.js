5c21455831f03dd2aa1ed66202fb5e356a29d201
Prabhjotsi2008 <prabhjotsi2008@gmail.com>
2026-03-23 11:15:26 +0530

    added theme-toggle functionality

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 const userInput = document.querySelector("#user-input");
-const searchButton = document.querySelector("button");
+const searchButton = document.querySelector("#search-btn");
 const userName = document.querySelector("#user-name");
 const userBio = document.querySelector("#user-bio");
 const userLocation = document.querySelector("#user-location");
@@ -10,13 +10,20 @@ const userImg = document.querySelector("#user-img");
 const loader = document.querySelector("#loader");
 const userLink = document.querySelector("#link");
 
+const themeToggle = document.querySelector("#theme-toggle");
+const toggleIcon = document.querySelector("#toggle-icon");
+
+const inputCont = document.querySelector(".input-cont");
+const titleBar = document.querySelector("#title-bar");
+const outputCont = document.querySelector(".output-cont");
+
 const userParas = document.querySelectorAll(".para");
 // console.log(userInput.placeholder,searchButton.textContent)
 
 const BASE_URL = "https://api.github.com/users/"
 
 searchButton.addEventListener("click", () => {
-    userImg.src = "images/github-img.png"
+    userImg.src = "images/github-svgrepo-com.svg";
     loader.classList.add("text-green-500")
     loader.textContent = "Fetching data from GitHub..."
     loader.classList.remove("hidden");
@@ -50,7 +57,7 @@ searchButton.addEventListener("click", () => {
         const followers = data.followers || 0;
         const following = data.following || 0;
         const repos = data.public_repos || 0;
-        const imgUrl = data.avatar_url || "images/github-img.png"; 
+        const imgUrl = data.avatar_url || "images/github-svgrepo-com.svg"; 
         const link = data.html_url || "#";
 
         loader.classList.add("hidden");
@@ -71,4 +78,20 @@ searchButton.addEventListener("click", () => {
         userParas.forEach(para => para.classList.add("hidden"));
     }
     },1500);
+});
+
+// Theme toggle functionality
+themeToggle.addEventListener("click", () => {
+    toggleIcon.src = toggleIcon.src.includes("dark-mode-github.svg") ? "images/light-mode-github.svg" : "images/dark-mode-github.svg";  
+
+    document.body.classList.toggle("bg-slate-100");
+    document.body.classList.toggle("text-slate-900");
+    inputCont.classList.toggle("bg-white");
+    inputCont.classList.toggle("border-slate-200");
+    titleBar.classList.toggle("bg-white");
+    titleBar.classList.toggle("border-slate-200");
+    themeToggle.classList.toggle("bg-white");
+    themeToggle.classList.toggle("border-slate-200");
+    outputCont.classList.toggle("bg-white");
+    outputCont.classList.toggle("border-slate-200");
 });
